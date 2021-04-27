6eaaaff7b42e8723f9c5d4960ee39c56efcc261a
Brian ONeill <Boneill3@users.noreply.github.com>
2021-04-27 12:07:59 -0700

    Create and destroy vcan bus closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use std::process::Command;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    create_bus("vcan0");
+    destroy_bus("vcan0");
+}
+
+fn create_bus(name: &str)
+{
+    let output = Command::new("sudo")
+    .arg("ip")
+    .arg("link")
+    .arg("add")
+    .arg("dev")
+    .arg(name)
+    .arg("type")
+    .arg("vcan")
+    .output()
+    .expect("failed to execute process");
+
+    if output.stderr.len() > 0
+    {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable create bus {}, it may already be created", name)
+    }
+
+    let output = Command::new("sudo")
+    .arg("ip")
+    .arg("link")
+    .arg("set")
+    .arg(name)
+    .arg("up")
+    .output()
+    .expect("failed to execute process");
+
+    if output.stderr.len() > 0
+    {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable to bring up bus {}", name)
+    }
+}
+
+fn destroy_bus(name: &str)
+{
+    let output = Command::new("sudo")
+    .arg("ip")
+    .arg("link")
+    .arg("del")
+    .arg("dev")
+    .arg(name)
+    .output()
+    .expect("failed to execute process");
+
+    if output.stderr.len() > 0
+    {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable to destroy bus {}", name)
+    }
 }
