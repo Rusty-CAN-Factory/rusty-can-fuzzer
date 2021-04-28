786580bbf7ce0c672d838bf36b28f617352cf41d
Brian ONeill <boneill3@gmail.com>
2021-04-27 19:36:04 -0700

    Create and destroy vcan bus closes (#5)
    * Create and destroy vcan bus closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::io::{self, Write};
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    create_bus("vcan0");
+    destroy_bus("vcan0");
+}
+
+/// Create a vcan bus using the following commands:
+/// sudo ip link add dev <name> type vcan
+/// sudo ip link set up <name>
+/// This function will panic if errors are returned
+fn create_bus(name: &str) {
+    let output = Command::new("sudo")
+        .arg("ip")
+        .arg("link")
+        .arg("add")
+        .arg("dev")
+        .arg(name)
+        .arg("type")
+        .arg("vcan")
+        .output()
+        .expect("failed to execute process");
+
+    if !output.stderr.is_empty() {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable to create bus {}, it may already be created", name)
+    }
+
+    let output = Command::new("sudo")
+        .arg("ip")
+        .arg("link")
+        .arg("set")
+        .arg(name)
+        .arg("up")
+        .output()
+        .expect("failed to execute process");
+
+    if !output.stderr.is_empty() {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable to bring up bus {}", name)
+    }
+}
+
+/// Destroy a vcan bus using the following commands:
+/// sudo ip link del dev <name>
+/// This function will panic if errors are returned
+fn destroy_bus(name: &str) {
+    let output = Command::new("sudo")
+        .arg("ip")
+        .arg("link")
+        .arg("del")
+        .arg("dev")
+        .arg(name)
+        .output()
+        .expect("failed to execute process");
+
+    if !output.stderr.is_empty() {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Unable to destroy bus {}", name)
+    }
 }
