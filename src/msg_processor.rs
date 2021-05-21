bd87f9dd09c68c31e0d858bf38418d49df6f0409
TheDragonMask <andrew.niven95@gmail.com>
2021-05-21 13:00:01 -0700

    Add Section struct, name data fields for both Section and SubSec, as well as basic traversal of all SubSecs in a test Section instance

--- src/msg_processor.rs
+++ src/msg_processor.rs
@@ -3,6 +3,7 @@ use rand::Rng;
 use socketcan::*;
 
 pub struct SubSec<'a> {
+    name: &'a str,
     num_bits: u8,
     //holes: &[u8],
     holes: &'a [u8],
@@ -12,10 +13,40 @@ pub struct SubSec<'a> {
 
 impl<'a> SubSec<'a> {
     pub fn display(self: &Self) {
-        println!("num_bits {}, holes {:?}, \n\
+        println!("{}: \n\
+                  num_bits {}, holes {:?}, \n\
                   is_specified {},  specified_val {}",
+                  self.name,
                   self.num_bits, self.holes, self.is_specified,
                   self.specified_val);
+    }
+}
+
+pub struct Section<'a> {
+    name: &'a str,
+    num_bytes: u8,
+    sub_secs: &'a [SubSec<'a>],
+    is_specified: bool,
+    specified_val: u8,
+}
+
+impl<'a> Section<'a> {
+    pub fn display(self: &Self) {
+        println!("{}: \n\
+                  num_bytes {}, is_specified {}, \n\
+                  specified_val {}",
+                  self.name,
+                  self.num_bytes, self.is_specified,
+                  self.specified_val);
+    }
+
+    pub fn display_sub_secs(self: &Self) {
+        //let mut i = 0;
+        //for i in self.sub_secs.len() {
+        for i in 0..self.sub_secs.len() {
+            println!("SubSec #{}: ", i);
+            self.sub_secs[i].display();
+        }
     }
 }
 
@@ -51,28 +82,54 @@ pub fn create_frame_send_msg(
 }
 
 pub fn msg_processor() -> u64 {
-    section_proc();
-    1
-}
-
-pub fn section_proc() -> u64 {
-    let test_sub_sec = SubSec {
-        num_bits: 3,
-        holes: &[1,2],
+    let test_section = Section {
+        name: "TestSec#1",
+        num_bytes: 1,
+        sub_secs: &[
+            SubSec {
+                name: "TestSubSec#1",
+                num_bits: 3,
+                holes: &[1,2],
+                is_specified: false,
+                specified_val: 0,
+            },
+            SubSec {
+                name: "TestSubSec#2",
+                num_bits: 5,
+                holes: &[5,6],
+                is_specified: false,
+                specified_val: 0,
+            },
+        ],
         is_specified: false,
         specified_val: 0,
     };
-    let result;
-    println!("test_sub_sec values: ");
-    test_sub_sec.display();
-    println!();
-    result = sub_sec_proc(test_sub_sec);
-    println!("sub_sec_proc random result (dec): {} ", result);
-    println!("sub_sec_proc random result (bin): {:#06b} ", result);
+    section_proc(&test_section);
     1
 }
 
-pub fn sub_sec_proc(sub_sec: SubSec) -> u8 {
+pub fn section_proc(section: &Section) -> u64 {
+    //let test_sub_sec = SubSec {
+    //    name: "TestSubSec#0",
+    //    num_bits: 3,
+    //    holes: &[1,2],
+    //    is_specified: false,
+    //    specified_val: 0,
+    //};
+    let mut result;
+    for i in 0..section.sub_secs.len() {
+        println!("########");
+        println!("test_sub_sec values: ");
+        section.sub_secs[i].display();
+        println!();
+        result = sub_sec_proc(&section.sub_secs[i]);
+        println!("sub_sec_proc result (dec): {} ", result);
+        println!("sub_sec_proc result (bin): {:#06b} ", result);
+    }
+    1
+}
+
+pub fn sub_sec_proc(sub_sec: &SubSec) -> u8 {
     let mut rng = rand::thread_rng();
     //-2 instead of -1 because for example 3^2-1=8,
     //but we can only fit up to 7 in 3 bits
