ae91c3abb4f0204bbf0262473c324f5fa48e1799
Brian ONeill <Boneill3@users.noreply.github.com>
2021-05-22 23:23:21 -0700

    Add json parser close #11, #20

--- src/json_config.rs
+++ src/json_config.rs
@@ -1,24 +1,38 @@
 use serde::{Deserialize, Serialize};
-use std::{fs, io};
+use std::{error, fs, io};
 
-#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
-pub struct Config {
-    data_type: String,
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
+pub struct SubSec {
+    num_bits: u8,
+    holes: Vec<u8>,
+    is_specified: bool,
+    specified_val: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Clone)]
+pub struct Section {
     name: String,
+    num_bytes: u8,
+    sub_secs: Vec<SubSec>,
+    is_specified: bool,
+    specified_val: u64,
 }
 
 /// Read configuration json file and return configuration object
-pub fn read_config(filename: &str) -> Config {
-    let file_data = fs::read_to_string(filename).unwrap();
-    let v: Config = serde_json::from_str(&file_data).unwrap();
-    v
+/*pub fn read_config(filename: &str) -> Result<Configrror>{
+    let file_data = Box::new(fs::read_to_string(filename).unwrap());
+    serde_json::from_str(&file_data);
+    //Ok(v)
+}*/
+pub fn read_config(filename: &str) -> Result<Section, Box<dyn error::Error>> {
+    let file_data = fs::read_to_string(filename)?;
+    serde_json::from_str(&file_data).map_err(|err| Box::new(err) as Box<dyn std::error::Error>)
 }
 
 /// Write configuration object to a json file
-pub fn save_config(filename: &str, config: &Config) -> Result<(), io::Error> {
+pub fn save_config(filename: &str, config: &Section) -> Result<(), io::Error> {
     let json_config = serde_json::to_string(config)?;
     fs::write(filename, json_config)?;
-
     Ok(())
 }
 
@@ -36,13 +50,30 @@ mod tests {
         let file_path = dir.path().join("test_output.json");
         let file_path_str = file_path.to_str().unwrap();
 
-        let config_values = Config {
-            data_type: "Type".to_string(),
-            name: "name".to_string(),
-        }; 
-        
-        save_config(file_path_str, &config_values).unwrap();
-        let input_values = read_config(file_path_str);
-        assert_eq!(input_values, config_values);
+        let sub_section1 = SubSec {
+            num_bits: 8,
+            holes: vec![],
+            is_specified: true,
+            specified_val: 0xFF,
+        };
+
+        let sub_section2 = SubSec {
+            num_bits: 8,
+            holes: vec![1, 2, 3],
+            is_specified: false,
+            specified_val: 0,
+        };
+
+        let section = Section {
+            name: String::from("SDO"),
+            num_bytes: 2,
+            sub_secs: vec![sub_section1, sub_section2],
+            is_specified: false,
+            specified_val: 0,
+        };
+
+        save_config(file_path_str, &section).unwrap();
+        let input_values = read_config(file_path_str).unwrap();
+        assert_eq!(input_values, section);
     }
 }
